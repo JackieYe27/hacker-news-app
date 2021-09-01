bcf0d5c839e88be933f0c4cbea3747b65503b681
Jackie Ye <jackieye27@gmail.com>
2021-08-31 21:43:58 -0700

    implement pagination and buttons

--- src/reducer.js
+++ src/reducer.js
@@ -22,8 +22,24 @@ const reducer = (state, action) => {
       return {...state, hits: newHits}
 
     case HANDLE_SEARCH:
-    return {...state, query: action.payload, page: 0}
+      return {...state, query: action.payload, page: 0}
 
+    case HANDLE_PAGE:
+      if (action.payload === "inc") {
+        let nextPage = state.page + 1;
+        if (nextPage > state.nbPages - 1) {
+          nextPage = 0;
+        }
+        return {...state, page:nextPage};
+      }
+      if (action.payload === "dec") {
+        let prevPage = state.page - 1;
+        if (prevPage < 0) {
+          prevPage = 0;
+        }
+        return {...state, page:prevPage};
+      }
+    // eslint-disable-next-line
     default:
       throw new Error(`no matching "${action.type}" action`);
   }
